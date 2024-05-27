6dd77c3be0336b9eb0ba3358a2bda0e5aeed5ebb
ThaboCodes <113469568+thaboxan@users.noreply.github.com>
2024-05-27 22:52:13 +0200

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = input("How old are you?")
+address = input('Where are you from?')
+
+print("Your name is " + name + " from " + address + " and you are " + age + " years old")
